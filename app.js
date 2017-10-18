ec41d00745d3fa12541429f90efd742914dc9a4d
kkepins1 <kfkepinski@gmail.com>
2017-10-18 18:11:42 +0200

    commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer, gamePlaying;
+var previousScore;
 
 init();
 
@@ -19,11 +20,12 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
     
-    document.getElementById('score-0').textContent = '0'
-    document.getElementById('score-1').textContent = '0'
-    document.getElementById('current-0').textContent = '0'
-    document.getElementById('current-1').textContent = '0'
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
     document.querySelector('.btn-roll').disabled = false;  
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
@@ -39,25 +41,32 @@ document.querySelector('.btn-new').addEventListener('click', init);
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         //1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
-
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+            
         //2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
         //3. Update the round score IF the rolled number was NOT a 1.
-        if(dice !== 1) {
+        
+
+        if(dice1 !== 1 && dice2 !==1) {
            //Add score  
-           roundScore += dice;
+           roundScore += (dice1 + dice2);
            document.querySelector('#current-' + activePlayer).textContent = roundScore; 
 
         } else {
            //Next player
             nextPlayer();    
-        } 
+        }
+       
     }  
 });
+
+
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if(gamePlaying) {
@@ -68,12 +77,21 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         //Update the UI
 
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
+        var finalScoreValue = document.querySelector('.final-score').value;
+        var winningScore;
+        
         //Check if player won the game
+        
+        if(finalScoreValue) {
+            winningScore = finalScoreValue;
+        } else {
+            winningScore = 100;
+        }
 
-        if(scores[activePlayer] >= 100) {
+        if(scores[activePlayer] >= winningScore ) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('#dice-1').style.display = 'none';
+            document.querySelector('#dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             document.querySelector('.btn-roll').disabled = true;
@@ -96,7 +114,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active'); 
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
 }
 
 
